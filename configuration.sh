bedbcbf6970abc985e02ea240b52c8b60fdd53f8
blarico <blaricov@gmail.com>
2023-12-12 11:46:38 +0000

    Date and time variables added

--- configuration.sh
+++ configuration.sh
@@ -5,6 +5,8 @@ OS="Unknown"
 NAME=""
 HOMEDIR=""
 NEWID=""
+DATE=""
+TIME=""
 
 # Find current operating system
 case "$(uname -s)" in
@@ -23,18 +25,24 @@ case "$OS" in
         NAME=$(whoami)
         HOMEDIR=$HOME
         NEWID=$(uuidgen)
+        DATE=$(date +%Y-%m-%d)
+        TIME=$(date +"%H:%M")
         ;;
     macOS)
         # macOS-specific command
         NAME=$(whoami)
         HOMEDIR=$HOME
         NEWID=$(uuidgen)
+        DATE=$(date +%Y-%m-%d)
+        TIME=$(date +"%H:%M")
         ;;
     Windows)
         # Windows-specific command (assuming you're in a Unix-like environment on Windows)
         NAME=$(cmd.exe /c 'echo %USERNAME%')
         HOMEDIR=$HOME
         NEWID=$(powershell.exe -Command "[guid]::NewGuid().toString()")
+        DATE=$(date +%Y-%m-%d)
+        TIME=$(date +"%H:%M")
         ;;
     *)
         echo "Operating System not identified or not supported."
@@ -46,3 +54,5 @@ echo "Operating System : $OS"
 echo "Username         : $NAME"
 echo "Home Directory   : $HOMEDIR"
 echo "NewGuid          : $NEWID"
+echo "Date             : $DATE"
+echo "Time             : $TIME"
